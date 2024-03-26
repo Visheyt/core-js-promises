ee5f77cd67fec7cb45f7f77094a742bc9776ad99
Alex <fornikaras7@gmail.com>
2024-03-26 17:05:23 +0500

    feat: finis first ex

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,15 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  if (number < 0) {
+    return new Promise((res, rej) => {
+      rej();
+    });
+  }
+  return new Promise((res) => {
+    res();
+  });
 }
 
 /**
